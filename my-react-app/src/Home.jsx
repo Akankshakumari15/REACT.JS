789123c675f971235253e7230c9918c2e5e1d486
Akankshakumari15 <a.akanksha050915@gmail.com>
2025-11-16 17:18:09 +0530

    React

---
+++ my-react-app/src/Home.jsx
@@ -0,0 +1,236 @@
+// import React, { useState } from 'react'
+
+// const Home = () => {
+
+
+
+
+//     // function fun1(){
+//     //     SetCount(count+1)
+//     // }
+//     // function fun2(){
+//     //     SetCount(count-1)
+//     // }
+//     // function fun3(){
+//     //     SetCount(0)
+//     // }
+//     let [count, SetCount]=    useState()
+//     setInterval(()=>{
+//         SetCount(new Date().toLocaleTimeString())
+//     })
+
+//   return (
+//     <div>
+//         <h2> {count}</h2>
+
+//         {/* <button onClick={()=>SetCount(count+1)}>++</button>
+//         <button onClick={()=>SetCount(count-1)}>--</button>
+//         <button onClick={()=>SetCount(0)}>reset</button> */}
+
+//     </div>
+//   )
+// }
+
+// export default Home
+
+
+
+// import React from 'react'
+
+// const Home = () => {
+//   let arr=[{id:1,name:"ankit"}]
+
+//   return (
+//     <div>
+//       {
+//         arr.map((a,b,c)=>{
+//           return(<div>
+//             <h1>{a.name}</h1>
+//           </div>)
+
+//         })
+//       }
+//     </div>
+//   )
+// }
+
+// export default Home
+
+
+// import React, { useEffect, useState } from 'react'
+// import   './App.css'
+// const Home = () => {
+//      let [ApiData,SetApiData]=useState([])
+//      useEffect(()=>{
+//       fetch('https://jsonplaceholder.typicode.com/todos')
+//       .then((res)=>{
+//         return res.json()
+
+//       }).then((data)=>{
+//         console.log(data);
+//         SetApiData(data)  
+//       })
+//      },[])       
+//   return (
+//     <div>
+
+// {
+//   ApiData.map((a)=>{
+//     return(<>
+//     <p id='one'>{a.id}</p>
+//     <h2> {a.title}</h2>
+//     </>)
+
+//   })
+// }
+
+//       {/* <h3>{count}</h3>
+//       <button onClick={()=>SetCount(count+1)}>click</button>
+//       <h3>{city}</h3>
+//       <button onClick={()=>{SetCity("bhopal")}}>Change</button> */}
+//     </div>
+//   )
+// }
+
+// export default Home
+
+// https://dummyjson.com/recipes
+
+
+// import React, { useEffect, useState } from 'react'
+// import './App.css'
+// import { Link } from 'react-router-dom'
+// const Home = ({cart,SetCart,  apiData,SetData,filteredddData,SetFilteredddData}) => {
+
+
+//       let arr=[1,2,3,4,5]
+//       let arr1=[6,7,8,...arr]
+//       console.log(arr1,"rrrr");
+
+
+// console.log("heheheheh");
+
+//   useEffect(()=>{
+//     fetch("https://dummyjson.com/recipes").then((res)=>{
+//       return res.json()
+
+//     }).then((data)=>{
+//       console.log(data.recipes);
+//       SetData(data.recipes)
+//       SetFilteredddData(data.recipes)
+
+
+//     })
+
+//   },[])
+
+//   function deletee(id){
+//     // console.log(id);
+//   let newArrr=   apiData.filter((a,b)=>{
+//       return  b!==id
+
+//     })
+//     SetData(newArrr)
+
+
+//   }
+//   function fun1(){
+//    let Data1=  [...apiData].sort((a,b)=>{
+//       return a.rating-b.rating
+
+//     })
+//     console.log(Data1,"SORTEDDDDD");
+
+//     SetData(Data1)
+
+
+//   }
+//   function fun2(){
+//     let Data1=  [...apiData].sort((a,b)=>{
+//       return b.rating-a.rating
+
+//     })
+//     console.log(Data1,"SORTEDDDDD");
+
+//     SetData(Data1)
+
+//   }
+
+//   function lunch(searchValue){
+//    let filtereddData=   apiData.filter((a)=>{
+//       return a.mealType[0]==searchValue
+
+//     })
+//     SetFilteredddData(filtereddData)
+
+//   }
+
+//   function dinner(a){
+//     let filtereddData=   apiData.filter((a)=>{
+//       return a.mealType[0]==a
+
+//     })
+//     SetFilteredddData(filtereddData)
+
+
+//   }
+//   return (
+//       <> 
+//       <Link  to={'/cart'}>  
+//        <button>add to Cart {cart.length}</button>
+//        </Link>
+//     <div>
+
+//       <button onClick={fun1}>acs</button>
+//       <button onClick={fun2}>des</button>
+//       <button onClick={()=>lunch("Lunch")}>lunch</button>
+//       <button onClick={()=>dinner("Dinner")}>dinner</button>
+//       <button>breakfast</button>
+
+
+//       {
+//         filteredddData.map((a,index)=>{
+//           return(<>
+//           <div id='card'>
+//           <img  src={a.image}/>
+//             <p>{a.name}</p>
+//             <p> Rating: {a.rating}</p>
+//          <button  onClick={()=> SetCart([...cart,a])}>add </button>
+
+//           </div>
+//           </>)
+//         })
+//       }
+//     </div>
+//     </>
+//   )
+// }
+
+// export default Home
+
+
+// #####
+import React, { useContext } from 'react';
+import context from './Context';
+import './App.css'
+
+const Home = () => {
+  const { state, dispatch } = useContext(context);
+
+  return (
+    <div id="container" >
+      {state.apiData.map((a, index) => (
+        <div   key={a.id || index}   id="card" >
+          <img src={a.image} alt="" />
+          <p>{a.name}</p>
+          <h5>MealType::{a.mealType}</h5>
+          <h4>Rating::{a.rating}</h4>
+          <button id='add' onClick={()=>dispatch({type:"ADD_TO_CART", payload:a})}>Add</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Home;
+
